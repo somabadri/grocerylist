90352181f6af324a4f0a67ad43c6d79faa38450d
Soma Sai Kumar Badri <sobadri@ucsc.edu>
2022-02-25 03:41:08 -0800

    new version ui update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,109 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import { TextField, Button, Typography, Dialog, DialogTitle, DialogContent, DialogActions, capitalize } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import ClearAllIcon from '@mui/icons-material/ClearAll';
+import SendIcon from '@mui/icons-material/Send';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Axios from 'axios';
+import ItemCard from './ItemCard';
 
 function App() {
-  const [item, setItem] = useState("");
-  const [amount, setAmount] = useState(0);
-  const [store, setStore] = useState("");
-  const [itemList, setItemList] = useState([]);
+  let theme = createTheme({
+    palette: {
+      type: 'light',
+      primary: {
+        main: '#356859',
+      },
+      primarysecond: {
+        main: '#37966F',
+      },
+      primarythird: {
+        main: '#B9E4C9',
+      },
+      secondary: {
+        main: '#fd5523',
+      },
+      background: {
+        main: '#FFFBE6',
+      },
+    },
+    typography: {
+      h1: {
+        fontFamily: 'Montserrat',
+        fontSize: 96,
+        fontWeight: 600,
+      },
+      h2: {
+        fontFamily: 'Montserrat',
+        fontSize: 60,
+        fontWeight: 600,
+      },
+      h3: {
+        fontFamily: 'Montserrat',
+        fontSize: 48,
+        fontWeight: 600,
+      },
+      h4: {
+        fontFamily: 'Montserrat',
+        fontSize: 34,
+        fontWeight: 600,
+      },
+      h5: {
+        fontFamily: 'Montserrat',
+        fontSize: 24,
+        fontWeight: 500,
+      },
+      h6: {
+        fontFamily: 'Lekton',
+        fontSize: 21,
+        fontWeight: 700,
+      },
+      subtitle1: {
+        fontFamily: 'Lekton',
+        fontSize: 21,
+        fontWeight: 700,
+      },
+      subtitle2: {
+        fontFamily: 'Lekton',
+        fontSize: 15,
+        fontWeight: 700,
+      },
+      body1: {
+        fontFamily: 'Montserrat',
+        fontSize: 16,
+        fontWeight: 600,
+      },
+      body2: {
+        fontFamily: 'Montserrat',
+        fontSize: 14,
+      },
+      button: {
+        fontFamily: 'Montserrat',
+        fontSize: 14,
+        fontWeight: 700,
+        textTransform: capitalize,
+      },
+      caption: {
+        fontFamily: 'Montserrat',
+        fontSize: 12,
+        fontWeight: 500,
+      },
+      overline: {
+        fontFamily: 'Montserrat',
+        fontSize: 10,
+        textTransform: capitalize,
+      },
+    },
+  });
+
+  const itemListMock = [
+    { amount: 1, store: "Store 1", item: "Item 1" },
+    { amount: 2, store: "Store 2", item: "Item 2" },
+    { amount: 3, store: "Store 3", item: "Item 3" },
+    { amount: 3, store: "Store 3", item: "Item 3" },
+    { amount: 3, store: "Store 3", item: "Item 3" },
+  ]
 
   const addItem = () => {
     Axios.post('https://grocerylist-maker.herokuapp.com/create', {
@@ -40,52 +137,75 @@ function App() {
   };
 
   useEffect(() => {
-    getItems();
+    // getItems();
   });
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [item, setItem] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [store, setStore] = useState("");
+  const [itemList, setItemList] = useState([]);
+
   return (
-    <div className="App">
-      <div className="fields">
-        <div className="inputField">
-          <label>Item:</label>
-          <input type="text"
-            onChange={(event) => {
-              setItem(event.target.value)
-            }} />
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Typography variant="h1" color="secondary">
+          Welcome Soma!
+        </Typography>
+        <div className="buttonsGroup">
+          <Button className="buttonInsideGroup" variant="contained" onClick={handleOpen} startIcon={<AddIcon />}>Add Item</Button>
+          <Button className="buttonInsideGroup" variant="contained" onClick={emptyList} startIcon={<ClearAllIcon />}>Clear List</Button>
+          <Button className="buttonInsideGroup" variant="contained" onClick={sendList} startIcon={<SendIcon />} >Send List</Button>
         </div>
-        <div className="inputField">
-          <label>Amount:</label>
-          <input type="number" onChange={(event) => {
-            setAmount(event.target.value)
-          }} />
-        </div>
-        <div className="inputField">
-          <label>Store:</label>
-          <input type="text"
-            onChange={(event) => {
-              setStore(event.target.value)
-            }} />
-        </div>
-        <button onClick={addItem}>Add Item</button>
-      </div>
-      <div className="groceryList">
-        <div className="headings">
-          <div className="item">Item</div>
-          <div className="amount">Amount</div>
-          <div className="store">Store</div>
-        </div>
-        {itemList.map((val, key) => {
-          return <div className="groceryItem">
-            <div className="item">{val.item}</div>
-            <div className="amount">{val.amount}</div>
-            <div className="store">{val.store}</div>
+        <div className='main-container'>
+          <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Add Item</DialogTitle>
+            <DialogContent>
+              <div className="fields">
+                <TextField
+                  label="Item"
+                  variant="outlined"
+                  margin="dense"
+                  type="text"
+                  onChange={(event) => {
+                    setItem(event.target.value)
+                  }} />
+                <TextField
+                  label="Amount"
+                  type="number"
+                  margin="dense"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  variant="outlined"
+                  onChange={(event) => {
+                    setAmount(event.target.value)
+                  }} />
+                <TextField label="Store"
+                  variant="outlined"
+                  type="text"
+                  margin="dense"
+                  onChange={(event) => {
+                    setStore(event.target.value)
+                  }} />
+              </div>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={addItem} variant="contained" >Add Item</Button>
+            </DialogActions>
+          </Dialog>
+          <div className="groceryList">
+            {itemListMock.map((val, key) => {
+              return <div className="groceryItem">
+                <ItemCard item={val.item} amount={val.amount} store={val.store} />
+              </div>
+            })}
           </div>
-        })}
-        <div className="clearButton">
-          <button onClick={emptyList}>Clear List</button>
-          <button onClick={sendList}>Send List</button>
         </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
